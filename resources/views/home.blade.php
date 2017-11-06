1690dc047e8f4b829a53389e0bbcfd27f83edfc6
Nikhil Agarwal <nikhilagarwal0963@gmail.com>
2017-11-05 18:26:17 -0800

    refresh

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Accounts</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -14,15 +14,12 @@
                         </div>
                     @endif
 
-                    <h1>Accounts</h1>
                     @foreach($users as $user)
                         <ul>
                             <li><a href="/users/{{$user->id}}">{{$user->name}}</a></li>
                         </ul>
                     @endforeach
-
-
-
+                    
                 </div>
             </div>
         </div>
