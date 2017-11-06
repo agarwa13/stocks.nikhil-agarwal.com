507bb476f19963f37442d8d392346c10eb93cd11
Nikhil Agarwal <nikhilagarwal0963@gmail.com>
2017-11-05 18:55:12 -0800

    updated color

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -52,7 +52,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-success">
+                <div class="panel panel-info">
                     <div class="panel-heading">{{$user->name}}'s Current Portfolio</div>
 
                     <div class="panel-body">
